68517f138a3f1417a26150941a9a80efb923a8e5
Raj Prakash Maurya <raj232033@gmail.com>
2023-04-26 05:45:24 +0100

    hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
