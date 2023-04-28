d358e2fd4f732f7b9491e9a189051e94df4bb4db
Raj Prakash Maurya <raj232033@gmail.com>
2023-04-28 12:02:08 +0100

    work section done

---
+++ src/Layout/WorkSection/index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import WorkCard from "src/components/Work/WorkCard";
+import Location from "../../Assets/Images/WorkImage/Location.svg";
+import Order from "../../Assets/Images/WorkImage/Order.svg";
+import Pay from "../../Assets/Images/WorkImage/Bill_Slip.svg";
+import Meal from "../../Assets/Images/WorkImage/Donut.svg";
+import "./WorkSection.css";
+const WorkCards = [
+  {
+    img: {
+      png: Location,
+    },
+    title: "Select Location",
+    content: "Choose the location where your food will be delivered",
+    size: "50px",
+  },
+  {
+    img: {
+      png: Order,
+    },
+    title: "Choose order",
+    content: "Check over hunderds of menus to pick your favorite food",
+  },
+  {
+    img: {
+      png: Pay,
+    },
+    title: "Pay advanced",
+    content: "It's quick , safe and simple. Select several methods of payment",
+  },
+  {
+    img: {
+      png: Meal,
+    },
+    title: "Enjoy meals",
+    content: "Food is made and delivered directly to your home",
+  },
+];
+
+function WorkSection() {
+  return (
+    <div className=" workSectionContainer">
+      <div className="workSectionHead text-center">How does it's work</div>
+      <div className="container workSection">
+        {WorkCards.map((item) => (
+          <WorkCard
+            key={item.work}
+            img={item.img.png}
+            tittle={item.title}
+            content={item.content}
+            size={item.size}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default WorkSection;
