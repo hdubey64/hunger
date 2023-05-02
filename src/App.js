6ae9a6eeae8145c7a89caa7b13cbf51120f77c9a
Vishal Dubey <hdubey64@gmail.com>
2023-05-02 15:34:28 +0530

    Fixesd Issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import OfferSection from "./Layout/OfferSection";
-import Navbar from "./components/Navbar";
+import DrawerAppBar from "./components/Navbar";
 import Hero from "./components/Hero/Hero";
+import OfferSection from "./Layout/OfferSection";
 import WorkSection from "./Layout/WorkSection";
 import PopularItemSection from "./Layout/PopularItemSection";
 import FeaturedSection from "./Layout/FeaturedSection";
-import DrawerAppBar from "./components/Navbar";
+
 
 function App() {
   return (
