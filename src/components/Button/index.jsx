e911fe24c9b5e1d31e3e6fcb89f6d1521bb429cd
Raj Prakash Maurya <raj232033@gmail.com>
2023-05-02 20:20:53 +0100

    search by food is done

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import "./Button.css";
 
 function IconButton({ label, color, onClick, icon, iconPostion = "start" }) {
-   return (
-      <button className={`iconBtn ${color}`} onClick={onClick}>
-         {icon && iconPostion === "start" && (
-            <span className="icon">{icon}</span>
-         )}
+  return (
+    <button className={`iconBtn ${color}`} onClick={onClick}>
+      {icon && iconPostion === "start" && <span className="icon">{icon}</span>}
 
-         {label}
-         {icon && iconPostion === "end" && <span className="icon">{icon}</span>}
-      </button>
-   );
+      {label}
+      {icon && iconPostion === "end" && <span className="icon">{icon}</span>}
+    </button>
+  );
 }
 
 export default IconButton;
