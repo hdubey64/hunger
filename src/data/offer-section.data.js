0a4cde20d99c9ec297f4b0842f6d80ed2180923f
Vishal Dubey <hdubey64@gmail.com>
2023-05-04 11:27:27 +0530

    Offer Section Data

---
+++ src/data/offer-section.data.js
@@ -0,0 +1,31 @@
+import PicOne from "src/Assets/Images/Image-15.png";
+import PicTwo from "src/Assets/Images/Image-10.png";
+import PicThree from "src/Assets/Images/Image-25.png";
+import PicFour from "src/Assets/Images/Image-20.png";
+
+export default [
+   {
+      offer: "15",
+      img: PicOne,
+      cardHead: "Greys Vage",
+      offerRemainingTime: "6 Days Remaining",
+   },
+   {
+      offer: "10",
+      img: PicTwo,
+      cardHead: "Greys Vage",
+      offerRemainingTime: "6 Days Remaining",
+   },
+   {
+      offer: "25",
+      img: PicThree,
+      cardHead: "Greys Vage",
+      offerRemainingTime: "7 Days Remaining",
+   },
+   {
+      offer: "20",
+      img: PicFour,
+      cardHead: "Greys Vage",
+      offerRemainingTime: "8 Days Remaining",
+   },
+];
