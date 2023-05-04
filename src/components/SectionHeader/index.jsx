70755abaae847ca5d3996f6117d6e81716aec3dc
Vishal Dubey <hdubey64@gmail.com>
2023-05-04 20:43:33 +0530

    Popular Section Header Fixed

--- src/components/SectionHeader/index.jsx
+++ src/components/SectionHeader/index.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import "./sectionHead.css";
 
 const SectionHeader = ({ Text, className }) => {
-
-
-  return (
-    <div className={`container sectionTag ${className}`}>
-      <h1 className=" fw-bold mb-5 fs-1">{Text}</h1>
-    </div>
-  );
-
+   return (
+      <div className={`container sectionTag ${className}`}>
+         <h1 className=" fw-bold mb-5 fs-1">{Text}</h1>
+      </div>
+   );
 };
 
 export default SectionHeader;
