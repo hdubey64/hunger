eb7d21bd0779699abaa2e30bd82f193676e6e811
Raj Prakash Maurya <raj232033@gmail.com>
2023-05-05 15:21:12 +0100

    search by food section is done

--- src/Layout/SearchByFoodSection/index.jsx
+++ src/Layout/SearchByFoodSection/index.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { useRef } from "react";
+import "./SearchByFoodSection.css";
+import SectionHeader from "src/components/SectionHeader";
+import { ArrowButton } from "src/components";
+import { FaAngleRight } from "react-icons/fa";
+import RightArrow from "src/Assets/Images/Icons/IconRightArrow.svg";
+import LeftArrow from "src/Assets/Images/Icons/IconLeftArraow.svg";
+import searchFoodSectionData from "src/data/searchFood-section.data";
+import SearchByFood from "src/components/SearchByFood";
 
 const SearchByFoodSection = () => {
-  return <div>hello</div>;
+  const scrollContainerSearchRef = useRef(null);
+  const handleLeftButtonClick = () => {
+    scrollContainerSearchRef.current.scrollLeft -= 300;
+  };
+  const handleRightButtonClick = () => {
+    scrollContainerSearchRef.current.scrollLeft += 300;
+  };
+
+  return (
+    <div className=" searchMain">
+      <div className="container">
+        <div className="searchSub ">
+          <header>
+            <SectionHeader Text="Search By Food" />
+          </header>
+          <div className="searchView">
+            <p>
+              View All <FaAngleRight />
+            </p>
+            <div className="arrowView">
+              <ArrowButton
+                img={LeftArrow}
+                customClass="sliderButton"
+                onClick={handleLeftButtonClick}
+              />
+              <ArrowButton
+                img={RightArrow}
+                customClass="sliderButton"
+                onClick={handleRightButtonClick}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="searchFoodMain">
+          <div className="scroll-container" ref={scrollContainerSearchRef}>
+            <div className="searchContent scroll_content">
+              {searchFoodSectionData.map((item) => (
+                <SearchByFood data={item} key={item.searchItems} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchByFoodSection;
