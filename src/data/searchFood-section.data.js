e48e196f416e7d30ee7427a6830d18baa1bed0b9
Raj Prakash Maurya <raj232033@gmail.com>
2023-05-05 05:39:27 +0100

    constant data is add in data file for serarchBy

---
+++ src/data/searchFood-section.data.js
@@ -0,0 +1,33 @@
+import PickOne from "../Assets/Images/SearchByFood/Image.svg";
+import PickTwo from "../Assets/Images/SearchByFood/Image (1).svg";
+import PickThree from "../Assets/Images/SearchByFood/Image(2).svg";
+import PickFour from "../Assets/Images/SearchByFood/Image(3).svg";
+import PickFive from "../Assets/Images/SearchByFood/Image(4).svg";
+import PickSix from "../Assets/Images/SearchByFood/Image(5).svg";
+
+export default [
+  {
+    img: PickOne,
+    cardHead: "Pizza",
+  },
+  {
+    img: PickTwo,
+    cardHead: "Burger",
+  },
+  {
+    img: PickThree,
+    cardHead: "Noodles",
+  },
+  {
+    img: PickFour,
+    cardHead: "Sub-sandwch",
+  },
+  {
+    img: PickFive,
+    cardHead: "Chowmein",
+  },
+  {
+    img: PickSix,
+    cardHead: "Steak",
+  },
+];
