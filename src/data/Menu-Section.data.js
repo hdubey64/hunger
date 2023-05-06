e8a490fe715ea29bed0012309f1409d4bb281ad6
Raj Prakash Maurya <raj232033@gmail.com>
2023-05-06 07:30:59 +0100

    menu section done

--- src/data/Menu-Section.data.js
+++ src/data/Menu-Section.data.js
@@ -1,7 +1,24 @@
+import Pizza from "../Assets/Images/MenuImage/pizza.png";
+import Chicken from "../Assets/Images/MenuImage/chicken.png";
+import sandwch from "../Assets/Images/MenuImage/sandwch.png";
+
 export default [
   {
-    img: "#",
-    header: "jsdhgk",
-    para: "hjgasd",
+    img: Pizza,
+    header: "Best deals ",
+    header2: "Crispy Sandwiches",
+    para: "Enjoy the large size of sandwiches. Complete perfect slice of sandwiches",
+  },
+  {
+    img: Chicken,
+    header: "Celebrate  parties with ",
+    header2: "Fried Chicken",
+    para: "Get the best fried chicken smeared with a lip smacking lemon chili flavor. Check out best deals for fried chicken.",
+  },
+  {
+    img: sandwch,
+    header: "Wanna eat hot & spicy",
+    header2: " Pizza?",
+    para: "Pair up with a friend and enjoy the hot and crispy pizza pops. Try it with the best deals.",
   },
 ];
