6db80b7bf33bee85a60fa83728afbf56ff88874b
Vishal Dubey <hdubey64@gmail.com>
2023-05-07 09:44:52 +0530

    Menu Section Container Fixed

--- src/Layout/MenuSection/MenuSection.jsx
+++ src/Layout/MenuSection/MenuSection.jsx
@@ -5,32 +5,37 @@ import IconButton from "src/components/Button";
 import { MenuSectionData } from "src/data";
 
 const MenuSection = () => {
-  return (
-    <div className="menu-main">
-      <div className="container">
-        {MenuSectionData.map((item) => (
-          <div className="menu-sub">
-            <div className="menu-left">
-              <div className="menu-left-content">
-                <h1>
-                  {item.header}
-                  <span className="content-head">{item.header2}</span>{" "}
-                </h1>
-                <p>{item.para}</p>
-              </div>
-              <div className="menu-left-button">
-                <IconButton label="proceed to order" color="order-btn" />
-              </div>
-            </div>
+   return (
+      <div className="menu-main container">
+         <div className="menuSectionMain">
+            {MenuSectionData.map((item) => (
+               <div className="menu-sub">
+                  <div className="menu-left">
+                     <div className="menu-left-content">
+                        <h1>
+                           {item.header}
+                           <span className="content-head">
+                              {item.header2}
+                           </span>{" "}
+                        </h1>
+                        <p>{item.para}</p>
+                     </div>
+                     <div className="menu-left-button">
+                        <IconButton
+                           label="proceed to order"
+                           color="order-btn"
+                        />
+                     </div>
+                  </div>
 
-            <div className="menu-right">
-              <img src={item.img} alt="" />
-            </div>
-          </div>
-        ))}
+                  <div className="menu-right">
+                     <img src={item.img} alt="" />
+                  </div>
+               </div>
+            ))}
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default MenuSection;
