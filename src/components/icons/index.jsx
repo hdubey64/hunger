3cd2a5773eb766ee2798aae5a73f7719bf11073d
Vishal Dubey <hdubey64@gmail.com>
2023-05-07 14:12:55 +0530

    Icons Imported in Icon Index

--- src/components/icons/index.jsx
+++ src/components/icons/index.jsx
@@ -1,1 +1,6 @@
 export { default as LeftArrowIcon } from "./LeftArrowIcon";
+export { default as DissountIcon } from "./DisscountIcon";
+export { default as TracingIcon } from "./TracingIcon";
+export { default as DeliveryIcon } from "./DeliveryIcon";
+export { default as PlayStoreIcon } from "./PlayStoreIcon";
+export { default as AppStoreIcon } from "./AppleStoreIcon";
