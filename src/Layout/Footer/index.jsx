b461621c6fe89060dea33921304228730b09478f
Vishal Dubey <hdubey64@gmail.com>
2023-05-08 10:57:20 +0530

    Footer Folder SetIp Done

---
+++ src/Layout/Footer/index.jsx
@@ -0,0 +1,13 @@
+import "./footer.css";
+
+const Footer = () => {
+   return (
+      <div className="footerSection">
+         <div className="footerSectionMain">
+            <h1 className="text-center">Footer</h1>
+         </div>
+      </div>
+   );
+};
+
+export default Footer;
