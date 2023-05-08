fd2f6c1c8f40c5a50cd6028b0c714a7e155e2baf
Vishal Dubey <hdubey64@gmail.com>
2023-05-08 10:38:56 +0530

    Added  Class At Btn iconEnd for End Icon Padding

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import "./Button.css";
 
 function IconButton({ label, color, onClick, icon, iconPostion = "start" }) {
-  return (
-    <button className={`iconBtn ${color}`} onClick={onClick}>
-      {icon && iconPostion === "start" && <span className="icon">{icon}</span>}
+   return (
+      <button className={`iconBtn ${color}`} onClick={onClick}>
+         {icon && iconPostion === "start" && (
+            <span className="icon">{icon}</span>
+         )}
 
-      {label}
-      {icon && iconPostion === "end" && <span className="icon">{icon}</span>}
-    </button>
-  );
+         {label}
+         {icon && iconPostion === "end" && (
+            <span className="icon iconEnd">{icon}</span>
+         )}
+      </button>
+   );
 }
 
 export default IconButton;
