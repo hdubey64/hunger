c8b5431dd06bb997563cf338bd92baff8c1e1de1
Raj Prakash Maurya <raj232033@gmail.com>
2023-05-11 18:44:29 +0100

    login form submit done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import IconButton from "../Button";
+import axios from "axios";
 
 function Login() {
   const Closebtn = () => {
     document.getElementById("cross").style.display = "none";
   };
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log("log:", formData);
+    const result = await axios.post("http://localhost:3000/login", formData);
+    console.log("result:", result.data);
+  }
+
   return (
     <div id="cross" className="loginMain">
-      <form action="" className="form">
+      <form onSubmit={handleSubmit} className="form">
         <div className="imgContainer">
           <span onClick={Closebtn}>&times;</span>
           <img
@@ -22,6 +35,10 @@ function Login() {
             type="email "
             placeholder="Enter Email address "
             name="username"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
             required
           />
 
@@ -32,6 +49,10 @@ function Login() {
             type="password"
             placeholder="Enter Password"
             name="password"
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
             required
           />
           <div className="formloginbtn">
