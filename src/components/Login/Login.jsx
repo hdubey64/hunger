beca68d72f4e23b0dded70ecdb3ed8385c2a2858
Raj Prakash Maurya <raj232033@gmail.com>
2023-05-11 09:52:45 +0100

    login is done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,52 +3,53 @@ import "./Login.css";
 import IconButton from "../Button";
 
 function Login() {
+  const Closebtn = () => {
+    document.getElementById("cross").style.display = "none";
+  };
   return (
-    <div className="container ">
-      <div className="loginMain">
-        <form action="" className="form">
-          <div className="imgContainer">
-            <span>&times;</span>
-            <img
-              src="https://www.w3schools.com/howto/img_avatar2.png"
-              alt="Avatar"
-            />
-          </div>
-          <div className="containerLogin">
-            {/* <label for="username">Username</label> */}
-            <input
-              type="email "
-              placeholder="Enter Email address "
-              name="username"
-              required
-            />
+    <div id="cross" className="loginMain">
+      <form action="" className="form">
+        <div className="imgContainer">
+          <span onClick={Closebtn}>&times;</span>
+          <img
+            src="https://www.w3schools.com/howto/img_avatar2.png"
+            alt="Avatar"
+          />
+        </div>
+        <div className="containerLogin">
+          {/* <label for="username">Username</label> */}
+          <input
+            type="email "
+            placeholder="Enter Email address "
+            name="username"
+            required
+          />
 
-            {/* <label for="password">
+          {/* <label for="password">
               // <b>Password</b>
             </label> */}
-            <input
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              required
-            />
-            <div className="formloginbtn">
-              <IconButton label="Login" color="formSubmitbtn" />
-              <label>
-                <input type="checkbox" name="remember" /> Remember me
-              </label>
-            </div>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            required
+          />
+          <div className="formloginbtn">
+            <IconButton label="Login" color="formSubmitbtn" />
+            <label>
+              <input type="checkbox" name="remember" /> Remember me
+            </label>
           </div>
-          <div className="container">
-            <div className="loginformFooter">
-              <IconButton label="Cancel" color="cancelbtn" />
-              <p>
-                <a href="#">Forget Password?</a>
-              </p>
-            </div>
+        </div>
+        <div className="container">
+          <div className="loginformFooter">
+            <IconButton onClick={Closebtn} label="Cancel" color="cancelbtn" />
+            <p>
+              <a href="#">Forget Password?</a>
+            </p>
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   );
 }
