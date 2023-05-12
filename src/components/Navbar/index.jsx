883fb4f944484f21a89e4a886a5c9b4b575912ed
Vishal Dubey <hdubey64@gmail.com>
2023-05-12 22:41:28 +0530

    Navbar Location change

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -17,118 +17,118 @@ const drawerWidth = 280;
 const navItems = ["Home", "About", "Contact"];
 
 function DrawerAppBar(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+   const { window } = props;
+   const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
+   const handleDrawerToggle = () => {
+      setMobileOpen((prevState) => !prevState);
+   };
 
-  const search = () => {
-    handleDrawerToggle();
-  };
-  ///For small screen
-  const drawer = (
-    <Box sx={{ textAlign: "center", bgcolor: "#fff" }}>
-      <List>
-        <div className="navsearch">
-          <input
-            type="text"
-            className="searchitems"
-            placeholder="search items"
-          />
-          <button onClick={search}>search</button>
-        </div>
-
-        <div onClick={handleDrawerToggle}>
-          <br />
-          <br />
-          <Link to="/login">
-            <button className="btn2">
-              <PersonIcon /> Login
-            </button>
-          </Link>
-        </div>
-      </List>
-    </Box>
-  );
-
-  //For large screen
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
-
-  return (
-    <Box sx={{ display: "flex" }}>
-      <AppBar component="nav" sx={{ bgcolor: "#fff", color: "black" }}>
-        <div className="navbar">
-          <Link to="/">
-            <div className="navbar-left">
-              HUN
-              <span className="ger">GER</span>
+   const search = () => {
+      handleDrawerToggle();
+   };
+   ///For small screen
+   const drawer = (
+      <Box sx={{ textAlign: "center", bgcolor: "#fff" }}>
+         <List>
+            <div className="navsearch">
+               <input
+                  type="text"
+                  className="searchitems"
+                  placeholder="search items"
+               />
+               <button onClick={search}>search</button>
             </div>
-          </Link>
 
-          <div className="navbar-mid">
-            <span className="del">Deliver to: </span> <img src={Map} /> Current
-            Location:
-            <span className="location"> Mohammadpur Bus Stand, Dhaka</span>
-          </div>
-          <div className="navbar-right">
-            <span className="sear">
-              <img src={Search} /> Search Food
-            </span>
-            <span>
-              <Link to="/login">
-                {" "}
-                <button className="btn">
-                  <FaUserAlt /> Login
-                </button>
-              </Link>
-            </span>
-          </div>
-          <div className="toggle">
-            <IconButton
-              edge="end"
-              color="inherit"
-              aria-label="menu"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-          </div>
-        </div>
-      </AppBar>
-      <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
+            <div onClick={handleDrawerToggle}>
+               <br />
+               <br />
+               <Link to="/login">
+                  <button className="btn2">
+                     <PersonIcon /> Login
+                  </button>
+               </Link>
+            </div>
+         </List>
       </Box>
-    </Box>
-  );
+   );
+
+   //For large screen
+   const container =
+      window !== undefined ? () => window().document.body : undefined;
+
+   return (
+      <Box sx={{ display: "flex" }}>
+         <AppBar component="nav" sx={{ bgcolor: "#fff", color: "black" }}>
+            <div className="navbar">
+               <Link to="/">
+                  <div className="navbar-left">
+                     HUN
+                     <span className="ger">GER</span>
+                  </div>
+               </Link>
+
+               <div className="navbar-mid">
+                  <span className="del">Deliver to: </span> <img src={Map} />{" "}
+                  Current Location:
+                  <span className="location"> XYZ, Indira Nagar, Lucknow</span>
+               </div>
+               <div className="navbar-right">
+                  <span className="sear">
+                     <img src={Search} /> Search Food
+                  </span>
+                  <span>
+                     <Link to="/login">
+                        {" "}
+                        <button className="btn">
+                           <FaUserAlt /> Login
+                        </button>
+                     </Link>
+                  </span>
+               </div>
+               <div className="toggle">
+                  <IconButton
+                     edge="end"
+                     color="inherit"
+                     aria-label="menu"
+                     onClick={handleDrawerToggle}
+                     sx={{ mr: 2 }}
+                  >
+                     <MenuIcon />
+                  </IconButton>
+               </div>
+            </div>
+         </AppBar>
+         <Box component="nav">
+            <Drawer
+               container={container}
+               variant="temporary"
+               open={mobileOpen}
+               onClose={handleDrawerToggle}
+               ModalProps={{
+                  keepMounted: true, // Better open performance on mobile.
+               }}
+               sx={{
+                  display: { xs: "block", sm: "none" },
+                  "& .MuiDrawer-paper": {
+                     boxSizing: "border-box",
+                     width: drawerWidth,
+                  },
+               }}
+            >
+               {drawer}
+            </Drawer>
+         </Box>
+      </Box>
+   );
 }
 
 DrawerAppBar.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
+   /**
+    * Injected by the documentation to work in an iframe.
+    * You won't need it on your project.
+    */
+   window: PropTypes.func,
 };
 
 export default DrawerAppBar;
